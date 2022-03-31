85165c6804b4b8a6868c83784ea4fff1592def56
sina2022 <91699531+sina2022@users.noreply.github.com>
2022-03-31 00:00:14 -0400

    updatet customerController

--- online-banking-system/app/Http/Controllers/customerController.php
+++ online-banking-system/app/Http/Controllers/customerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Session;
 use App\Models\account;
 use App\Models\contacts;
+use App\Models\transaction;
 use Illuminate\Http\Request;
 use App\Models\customer;
 class customerController extends Controller
@@ -51,11 +52,36 @@ class customerController extends Controller
     public function show($id)
     {    
       
-       $customer=customer::where('customer_id', $id)->get();
-        $accounts=account::where('customer_id', $id)->get();
-        $contacts=contacts::where('customer_id', $id)->get();
-        return view('show',compact('customer','accounts','contacts'));
-    }
+        $customer=customer::where('customer_id', $id)->get();
+         $accounts=account::where('customer_id', $id)->get();
+         $contacts=contacts::where('customer_id', $id)->get();
+ 
+         $tr=[];
+         $sendmoney=[];
+         $receivedmoney=[];
+         foreach($accounts as $account){
+             array_push($tr,$account);
+         }
+         foreach($tr as $account){
+           
+         $sendTransactions=transaction::join ('contacts', 'transactions.contact_id', '=', 'contacts.contact_id')
+         ->where('transactions.account_number', $account['account_number'])
+         ->get(['transactions.*','contacts.account_number as receiver_account_number']);
+         array_push($sendmoney,$sendTransactions); 
+         }
+         
+         foreach($tr as $account){
+             $receiveTransactions=transaction::join ('contacts', 'transactions.contact_id', '=', 'contacts.contact_id')
+             ->where('contacts.account_number', $account['account_number'])
+             ->get(['transactions.*','contacts.account_number as sender_account_number']);
+             array_push($receivedmoney,$receiveTransactions); 
+         }
+      
+       
+       return view('show',compact('customer','accounts','contacts','sendmoney','receivedmoney'));
+     
+ }
+ 
 
     /**
      * Show the form for editing the specified resource.
